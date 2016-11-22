8935631d7937a8e35166e53a08e531bea585d0d3
Arjun Kalburgi <askalburgi@users.noreply.github.com>
2016-11-21 23:22:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -26,21 +26,22 @@ def main():
         	5. Determine Functional Dependencies Equivalence\n \
         	6. Quit\n")
         
-        if action == "1":
-			relation_info.start()
-        elif action == "2":
-        	NF3_decomp.start()
-        elif action == "3":
-        	BCNF_decomp.start()
-        elif action == "4":
-        	attr_closure.start()
-        elif action == "5":
-        	func_dep.start()
-        elif action == "6":
-        	break;
-        else:
-            print("Unknown input, please type a number")
-    print("Bye")
+		if action == "1":
+				relation_info.start()
+		elif action == "2":
+			NF3_decomp.start()
+		elif action == "3":
+			BCNF_decomp.start()
+		elif action == "4":
+			attr_closure.start()
+		elif action == "5":
+			func_dep.start()
+		elif action == "6":
+			break;
+		else:
+	    		print("Unknown input, please type a number")
+
+	print("Bye")
 
 if __name__  == "__app__":
 	connect_database()
