6ce3a603ed9432c8a5efc291ef4f1f02b3fb41fe
Megasonic988 <weixiang@ualberta.ca>
2016-11-25 10:36:50 -0700

    table name remove BCNF_

--- database_files/database.py
+++ database_files/database.py
@@ -8,7 +8,7 @@ c = None # global for cursor
 def connectDB(path):
 	global conn
 	global c
-	if path == "": 
+	if path == "":
 		path = 'database_files/MiniProject2-InputExample.db'
 	conn = sqlite3.connect(path)
 	conn.row_factory = dict_factory
@@ -30,21 +30,21 @@ def printTableNames():
 		# print("\n")
 
 # relation_info.py
-def printFDSfor(table_name): 
+def printFDSfor(table_name):
 	t = table_name.split("_")
 	c.execute("SELECT * FROM ?".replace("?", t[0]+"_FDS_"+t[1]))
-	for line in c: 
+	for line in c:
 		print(str(line['LHS']) + " -> " + str(line['RHS']))
 
-def attributesFromSql(sql): 
+def attributesFromSql(sql):
 	att_arr = sql.split("\n")
 	att_arr.pop(0)
 	att_arr.pop(len(att_arr)-1)
 
 	att = {}
-	for a in att_arr: 
+	for a in att_arr:
 		a = a.strip()
-		a = a.replace(",", "")		
+		a = a.replace(",", "")
 		a = a.split(" ")
 		# att.append({a[0]: a[1]})
 		att[a[0]] = a[1]
@@ -52,32 +52,32 @@ def attributesFromSql(sql):
 	return att
 
 # NF3_decomp
-def chooseTable(): 
+def chooseTable():
 	c.execute("SELECT name,sql FROM SQLITE_MASTER WHERE name NOT LIKE '%FDS%';")
 	tables = {}
-	for table in c: 
+	for table in c:
 		print("Table Name: " + str(table["name"]))
 		tables[table["name"]] = table["sql"]
 		# print("\n")
 
 	table = raw_input("Choose a table (type table's name)")
-	if table in tables: 
+	if table in tables:
 		print("This is the table (and its FDs) you've chosen: \n"+ tables[table])
 		return {'name': table, 'sql': tables[table], 'attributes': attributesFromSql(tables[table])}
-	else: 
+	else:
 		print("This is not a valid table name, please choose again.")
 
 	chooseTable()
 
 # NF3_decomp
 # returns fd sets in a table -> [{'LHS': 'AB', 'RHS': 'CD'}]
-def getFDSfor(table_name): 
+def getFDSfor(table_name):
 	t = table_name.split("_")
 	tlast = t[len(t)-1]
 	t.remove(tlast)
 	c.execute("SELECT * FROM ?".replace("?", "_".join(t)+"_FDS_"+tlast))
 	fds = []
-	for line in c: 
+	for line in c:
 		fds.append({'LHS': str(line['LHS']).replace(",",""), 'RHS': str(line['RHS']).replace(",","")})
 	return fds
 
@@ -100,7 +100,7 @@ def getFDSforTables(table_names):
 # getFDSforTables('Input_FDs_R1')
 
 
-def partition(f,t): 
+def partition(f,t):
 	'''
 		t: {name: "...", sql: '...', attributes: {'A': 'INT',...}}
 		f: [{LHS: '..', RHS:'..'}, {..}]
@@ -108,15 +108,15 @@ def partition(f,t):
 	name = "NF3_" + t['name'].replace("Input", "Output")
 
 	# each fd needs 1.Partition Table, 2.FDs Table
-	for fd in f: 
+	for fd in f:
 		fd_att = fd['LHS'] + fd['RHS'] # attributes of fd
 		t_name = name + "_" + fd_att
 		# print(t_name)
 		c.execute("DROP TABLE IF EXISTS " + t_name)
 
 		# 1. Partition Table
-		partition_sql = "CREATE TABLE " + t_name + "(\n" # bs i found online: + " AS SELECT " + ','.join(list(fd_att)) + " FROM " + t['name'] 
-		for at in list(fd_att): 
+		partition_sql = "CREATE TABLE " + t_name + "(\n" # bs i found online: + " AS SELECT " + ','.join(list(fd_att)) + " FROM " + t['name']
+		for at in list(fd_att):
 			partition_sql += "  " + at + " " + t['attributes'][at] + ",\n"
 		partition_sql += "  PRIMARY KEY("+",".join(fd["LHS"])+"))"
 
@@ -151,7 +151,7 @@ def create_new_schemas(schemas, name):
 	Output_FDS_R1_FA
 	'''
 	for s in schemas:
-		table_name = "BCNF_Output_" + name + '_' + ''.join(s['attributes'])
+		table_name = "Output_" + name + '_' + ''.join(s['attributes'])
 		c.execute("DROP TABLE IF EXISTS " + table_name)
 		c.execute("CREATE TABLE " + table_name + '(' + ','.join(s['attributes']) + ')')
 		fd_table_name = 'Output_FDS_' + name + '_' + ''.join(s['attributes'])
